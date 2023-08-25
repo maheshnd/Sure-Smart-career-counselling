9dad2e0d31815938a36040051e6a599333799f44
mahesh <deshmukh.mahesh.n@gmail.com>
2023-08-26 00:39:14 +0530

    deploy on github web pages

--- src/components/BrandName.js
+++ src/components/BrandName.js
@@ -1,6 +1,9 @@
 export const BrandName = () => {
   return (
-    <a className="navbar-brand" href="/">
+    <a
+      className="navbar-brand"
+      href="https://maheshnd.github.io/Sure-Smart-career-counselling/"
+    >
       <span className="branding" id="sure">
         <span className="animated-content branding">Sure</span>
       </span>
